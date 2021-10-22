c6a588ae11957f9c2e10718718525f859baaf1dc
fedesilveroutn <fedesilveromusico@gmail.com>
2021-10-22 17:57:37 -0300

    Creacion del repositorio

---
+++ RECU PARTE1/src/RECU PARTE1.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+Recuperatorio parte 1.
+Alumno: Federico Elías Silvero.
+Divisón: 1°D
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout, NULL);
+
+
+
+
+	return 0;
+}
